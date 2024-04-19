523e069da5918bb80ecb78b353ac83eb01ecec65
Nooruddin <160733113+Nooruddin23@users.noreply.github.com>
2024-04-19 10:49:59 -0700

    Created a calculator

---
+++ index.js
@@ -0,0 +1,28 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const anwser = await inquirer.prompt([
+    { message: "Enter firstNumber", type: "number", name: "firstNumber" },
+    { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    }
+]);
+//Conditional Statement
+if (anwser.operator === "Addition") {
+    console.log(anwser.firstNumber + anwser.secondNumber);
+}
+else if (anwser.operator === "Subtraction") {
+    console.log(anwser.firstNumber - anwser.secondNumber);
+}
+else if (anwser.operator === "Multiplication") {
+    console.log(anwser.firstNumber * anwser.secondNumber);
+}
+else if (anwser.operator === "Division") {
+    console.log(anwser.firstNumber / anwser.secondNumber);
+}
+else {
+    console.log("Please select a valid operator");
+}
--- index.ts
+++ index.ts
@@ -1,34 +1,31 @@
-#! usr/bin/env node
 
-import inquirer from "inquirer";
+ import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+ const anwser = await inquirer.prompt([
+  {message:"Enter firstNumber", type:"number", name:"firstNumber"},
+  {message:"Enter secondNumber", type:"number", name:"secondNumber"},
   {
-    message: "Select one of the operators to perform action",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
-]);
-console.log(answer);
+    message:"Select one of the operator",
+    type:"list",
+    name:"operator",
+    choices:["Addition", "Subtraction", "Multiplication", "Division"]
+  }
+ ])
 
+ //Conditional Statement
 
-//Conditional statement
-
-if (answer.operator === "Addition") {
-  console.log(answer.firstNumber + answer.secondNumber);
-} 
-else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-} 
-else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-} 
-else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
-} 
-else {
-  console.log("Please select a valid operator");
-}
+ if (anwser.operator === "Addition") {
+  console.log(anwser.firstNumber + anwser.secondNumber)
+ }
+ else if (anwser.operator === "Subtraction") {
+  console.log(anwser.firstNumber - anwser.secondNumber)
+ }
+ else if (anwser.operator === "Multiplication") {
+  console.log(anwser.firstNumber * anwser.secondNumber)
+ }
+ else if (anwser.operator === "Division") {
+  console.log(anwser.firstNumber / anwser.secondNumber)
+ }
+ else {
+  console.log("Please select a valid operator")
+ }
